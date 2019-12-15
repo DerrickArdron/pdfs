477adffa4307e3285d28090b1e5743fe0825c6a3
DerrickArdron <42250783+DerrickArdron@users.noreply.github.com>
2019-12-15 18:44:44 +0000

    Working properly for Methuen, including HDS rank types

--- pdfs.py
+++ pdfs.py
@@ -3,7 +3,7 @@ import dasql
 
 dasql.csvToDb("advanced_current_members_by_mshp_status.csv","contacts.db","Members")
 dasql.csvToDb('lodges.csv','contacts.db','Lodges', outputToFile=False)
-dasql.createTable('contacts.db', 'Ranks', 'Glref, Provincial, Unit, Rank, Start', 'Glref', 'Type', 'Provincial', 'Unit', 'Rank', 'Start', 'End')
+dasql.createTable('contacts.db', 'Ranks', 'Glref, Provincial, UnitID, Rank, Start', 'Glref', 'Type', 'Provincial', 'UnitID', 'UnitName', 'Rank', 'Start', 'End')
 
 con = sqlite3.connect('contacts.db')
 cur = con.cursor()
@@ -18,62 +18,95 @@ os.system('cls')
 pdffileobj = open('L631 Career Summaries.PDF', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdffileobj)
 page_count = (pdfReader.numPages)
-methuen_file = open('L631 Methuen.txt', 'w')
+lodge_pdf_txt = open('Lodge.txt', 'w')
 page_number = 0
 while page_number < page_count:
     pageObj = pdfReader.getPage(page_number)
     page_content = pageObj.extractText()
-    methuen_file.write(page_content)
+    lodge_pdf_txt.write(page_content)
     page_number += 1
-methuen_file.close()
-methuen_file = open('L631 Methuen.txt', 'r')
+lodge_pdf_txt.close()
+
+# Open the file and initiate variables
+lodge_pdf_txt = open('Lodge.txt', 'r')
 glref = ''
-org = ''
+office_type = ''
 pdf_section = ''
-counter = 0
-for line in methuen_file:
+prov_counter = -1
+loffice_counter = -1
+for line in lodge_pdf_txt:
     if line.strip() == 'Provincial Ranks':
         pdf_section = 'Provincial Ranks'
     elif line.strip() == 'Lodge Career':
         pdf_section = 'Lodge Career'
     elif line.strip() == 'Lodge Offices':
         pdf_section = 'Lodge Offices'
-    counter -= 1
+    prov_counter -= 1
+    loffice_counter -= 1
+
+    # Get te Glref
     if glref_regex.search(line):
-        line = line[:-2]
+        line = line.strip()
         stmt = 'SELECT "Gl ref" from Members where "Gl ref" = \'' + line +"'"
         cur.execute(stmt)
         con.commit()
         result = str(cur.fetchone())
         if result.upper() != 'NONE':
             glref = line
+
     # Process Provincial Ranks
-    if line[:-1] == 'Craft Rank':
-        org = 'Craft'
-    elif line[:-1] == 'Royal Arch Rank':
-        org = 'Chapter'
+    if line.strip() == 'Craft Rank':
+        office_type = 'PROVCRAFTRANK'
+    elif line.strip() == 'Royal Arch':
+        office_type = 'PROVCHAPRANK'
+    # Prepare Data for Provincial Rank
     if province_regex.search(line):
-        counter = 4
-        provincial_rank = line[:-1]
-    if counter == 2:
+        prov_counter = 4
+        provincial_rank = line.strip()
+    if prov_counter == 2:
         start_date = line.strip()
-    if counter == 0:
+    if prov_counter == 0:
         if date_regex.search(line):
-            end_date = line[:-1]
+            end_date = line.strip()
         else:
             end_date = ''
         if pdf_section == 'Provincial Ranks':
-            dasql.dataAdder('Provincial','contacts.db', 'Ranks', 'Glref',glref , Type=org, Provincial=provincial_rank, Start=start_date, End=end_date)
+            dasql.dataAdder('Provincial','contacts.db', 'Ranks', Glref=glref, Type=office_type, Provincial=provincial_rank, UnitID='', UnitName='', Rank='', Start=start_date, End=end_date)
+        #office_type = ''
         provincial_rank = ''
-        counter = -1
-    if lodge_id_regex.search(line):
-        line = line[:-1]
-        stmt = 'SELECT "Lc number" from Lodges where "Lc number" = \'' + line +"'"
+        prov_counter = -1
+
+
+    if pdf_section == "Lodge Offices" and lodge_id_regex.search(line):
+        line = line.strip()
+        stmt = 'SELECT "Lc name" from Lodges where "Lc number" = \'' + line +"'"
         cur.execute(stmt)
         con.commit()
         result = str(cur.fetchone())
         if result.upper() != 'NONE':
-            lodge = line
+            lodge_id = line
+            lodge_name = result[2:]
+            lodge_name = lodge_name[:-3]
+            if lodge_id[0:1] == 'L':
+                office_type = 'LODGEOFF'
+            elif lodge_id[0:1] == 'C':
+                office_type = 'CHAPOFF'
+            if lodge_name == 'Buckinghamshire Provincial Grand Stewards':
+                loffice_counter = 9
+            else:
+                loffice_counter = 8
+            # print(glref, pdf_section, lodge_id, lodge_name, loffice_counter)
+        else:
+            loffice_counter = -1
+    if loffice_counter == 4:
+        lodge_office = line.strip()
+    if loffice_counter == 2:
+        start_date = line.strip()
+    if loffice_counter == 0:
+        end_date = line.strip()
+    if pdf_section == 'Lodge Offices' and loffice_counter == 0:
+        dasql.dataAdder('Lodge Offices','contacts.db', 'Ranks', Glref = glref , Type=office_type, Provincial='', UnitID=lodge_id, UnitName=lodge_name, Rank=lodge_office, Start=start_date, End=end_date)
+        loffice_counter = -1
 
 dasql.output('contacts.db', 'Ranks', 'ranks.csv')
 
